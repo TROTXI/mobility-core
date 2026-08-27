608aa816385d57a63c815bfb4be1e81bb8e0c9fb
Fosu Adom <adomfosugit@gmail.com>
2026-08-27 22:10:08 +0000

    remove unused import from home_tab

--- apps/trotxi_commuter/lib/Presentations/Home/widgets/Tabs/home_tab.dart
+++ apps/trotxi_commuter/lib/Presentations/Home/widgets/Tabs/home_tab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:trotxi_client/trotxi_client.dart';
 import 'package:trotxi_commuter/core/config/theme/app_colors.dart';
 
